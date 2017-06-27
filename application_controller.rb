300ab78a5d02a72631a2b3d22451ccae2d23f8d4
Maddie Pfau <madbird67.joy@gmail.com>
2017-06-27 23:17:21 +0000

    For haley

--- application_controller.rb
+++ application_controller.rb
@@ -7,7 +7,15 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-
   
+  # get '/birthstone' do
+  #   erb :results
+  # end
+  
+  post '/birthstone' do
+    user_month=params[:month]
+    @answer=choose_gem(user_month)
+    erb :results
+  end
   
 end
--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,21 @@
-def choose_gem(month)
 
- 
+def choose_gem(key)
+    months_to_stone={
+    :January => "Garnet",
+    :February => "Amethyst",
+    :March => "Bloodstone",
+    :April => "Diamond",
+    :May => "Emerald",
+    :June => "Pearl",
+    :July => "Ruby",
+    :August => "Peridot",
+    :September => "Sapphire", 
+    :October => "Tourmeline",
+    :November => "Yellow Topaz",
+    :December => "Zircon"
+
+}
+months_to_stone[key.to_sym]
+
 end
 
